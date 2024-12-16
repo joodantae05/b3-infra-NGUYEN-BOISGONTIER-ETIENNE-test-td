39435d1924914ebef9d8fc5f7ebffb8b10ada315
joodantae05 <anguyen951@proton.me>
2024-12-16 15:35:34 +0100

    ajout de la fonction clear cart

--- cart.py
+++ cart.py
@@ -39,23 +39,14 @@ class Cart:
                                   for product, quantity in self.items.items()])
         total = self.calculate_total()
         return f"{cart_display}\nTotal (after discount): {total:.2f}€"
-
     def clear_cart(self):
         """Supprime tous les articles du panier"""
         if not hasattr(self, 'items') or not isinstance(self.items, (list, dict)):
             print("Erreur : Le panier n'existe pas ou n'est pas correctement défini.")
             return
-
         if len(self.items) == 0:
             print("Votre panier est déjà vide.")
         else:
             self.items.clear()
             print("Votre panier a été vidé.")
-    def checkout(self):
-        """Affiche le récapitulatif de la commande avant le paiement"""
-        if not self.items:
-            return "Your cart is empty. Please add items to your cart before proceeding to checkout."
-        self.apply_discount()  # Applique la remise avant le calcul du total
-        total = self.calculate_total()
-        return f"Proceeding to checkout...\n{self.display_cart()}\nTotal to pay: {total:.2f}€"
 
